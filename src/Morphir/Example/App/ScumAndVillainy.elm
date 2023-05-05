192b759abe711d9804ffcd22192850a09ee51e48
albx79 <albx79@gmail.com>
2023-05-05 14:22:46 +0200

    WIP improvements may not work, revert if needed

--- src/Morphir/Example/App/ScumAndVillainy.elm
+++ src/Morphir/Example/App/ScumAndVillainy.elm
@@ -3,11 +3,35 @@ module Morphir.Example.App.ScumAndVillainy exposing (..)
 import List exposing (filter, length, maximum)
 import Maybe exposing (withDefault)
 
---type alias Enum a = {
---    values: List a,
---    toString: a -> String }
+type alias Enum a = {
+    values: List a,
+    fst: a,
+    lst: a}
+ord : Enum a -> a -> Int
+ord e a =
+    e.values
+        |> List.indexedMap Tuple.pair
+        |> filter (\(_, item) -> item == a)
+        |> List.map Tuple.first
+        |> List.head
+        |> withDefault -1
+succClip : Enum a -> a -> a
+succClip e a =
+    let pos = (ord e a) + 1 in
+    e.values |> List.drop pos |> List.head |> withDefault e.lst
+
+predClip : Enum a -> a -> a
+predClip e a =
+    let pos = (ord e a) - 1 in
+    e.values |> List.drop pos |> List.head |> withDefault e.fst
 
 type SuccessLevel = Critical | Full | Partial
+successLevel : Enum SuccessLevel
+successLevel = {
+    values = [Partial, Full, Critical],
+    fst = Partial,
+    lst = Critical }
+
 type Outcome = Success SuccessLevel | Bad
 type Position = Controlled | Risky | Desperate
 type EffectLevel = Zero | Limited | Standard | Great | Extreme
